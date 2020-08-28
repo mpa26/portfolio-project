68ea1da72548d668a2475bc148a9c871564ed066
Pavel M <m-ars26@ya.ru>
2020-08-28 17:43:31 +0300

    add sources to project

---
+++ conftest.py
@@ -0,0 +1,23 @@
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+# run command: pytest -v --tb=line --language=en test_main_page.py
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language: en or ru")  # default=None
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    time.sleep(5)
+    print("\nquit browser..")
+    browser.quit()
+
